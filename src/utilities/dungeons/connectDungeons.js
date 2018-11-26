27d89350d43d1e97662236f3b1c15ff56ac7b489
Zach Toben <zach.toben@gmail.com>
2018-11-26 15:24:10 -0600

    remove logging of path

--- src/utilities/dungeons/connectDungeons.js
+++ src/utilities/dungeons/connectDungeons.js
@@ -31,7 +31,6 @@ export function connectDungeons(map, dungeons) {
             path.forEach(({x, y}) => {
               setTileType(map[x][y], FLOOR);
             });
-            console.log({path});
           }
         }
       );
