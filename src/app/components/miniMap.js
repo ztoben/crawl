3f4a6d4d5a64987affe10d31b8f06db62f05716d
Zach Toben <zach.toben@gmail.com>
2018-11-28 15:27:38 -0600

    add minimap label

--- src/app/components/miniMap.js
+++ src/app/components/miniMap.js
@@ -12,6 +12,7 @@ class MiniMap extends Component {
   render() {
     return (
       <div style={miniMapStyle}>
+        <h5 style={{margin: 0, textAlign: 'right'}}>minimap</h5>
         <Stage width={200} height={200}>
           <Layer>{this.props.miniMapArray}</Layer>
         </Stage>
