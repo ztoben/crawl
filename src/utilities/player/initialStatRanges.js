f0166c012b24741893ec01b93981c9abd775eb0f
Nic <ngrunks@gmail.com>
2018-11-29 20:44:54 -0600

    Added initial stat function based on class

---
+++ src/utilities/player/initialStatRanges.js
@@ -0,0 +1,132 @@
+export const ranges = {
+  paladin: {
+    hp: {
+      min: 75,
+      max: 100,
+    },
+    mp: {
+      min: 10,
+      max: 30,
+    },
+    atk: {
+      min: 15,
+      max: 20,
+    },
+    def: {
+      min: 10,
+      max: 15,
+    },
+    satk: {
+      min: 20,
+      max: 25,
+    },
+    sdef: {
+      min: 12,
+      max: 17,
+    },
+  },
+  rogue: {
+    hp: {
+      min: 75,
+      max: 100,
+    },
+    mp: {
+      min: 10,
+      max: 30,
+    },
+    atk: {
+      min: 15,
+      max: 20,
+    },
+    def: {
+      min: 10,
+      max: 15,
+    },
+    satk: {
+      min: 20,
+      max: 25,
+    },
+    sdef: {
+      min: 12,
+      max: 17,
+    },
+  },
+  thief: {
+    hp: {
+      min: 75,
+      max: 100,
+    },
+    mp: {
+      min: 10,
+      max: 30,
+    },
+    atk: {
+      min: 15,
+      max: 20,
+    },
+    def: {
+      min: 10,
+      max: 15,
+    },
+    satk: {
+      min: 20,
+      max: 25,
+    },
+    sdef: {
+      min: 12,
+      max: 17,
+    },
+  },
+  druid: {
+    hp: {
+      min: 75,
+      max: 100,
+    },
+    mp: {
+      min: 10,
+      max: 30,
+    },
+    atk: {
+      min: 15,
+      max: 20,
+    },
+    def: {
+      min: 10,
+      max: 15,
+    },
+    satk: {
+      min: 20,
+      max: 25,
+    },
+    sdef: {
+      min: 12,
+      max: 17,
+    },
+  },
+  cleric: {
+    hp: {
+      min: 75,
+      max: 100,
+    },
+    mp: {
+      min: 10,
+      max: 30,
+    },
+    atk: {
+      min: 15,
+      max: 20,
+    },
+    def: {
+      min: 10,
+      max: 15,
+    },
+    satk: {
+      min: 20,
+      max: 25,
+    },
+    sdef: {
+      min: 12,
+      max: 17,
+    },
+  },
+};
